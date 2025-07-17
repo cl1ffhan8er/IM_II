7009086546cd4174cf6af8e53980d3d16fd53719
jijianna <22101542@usc.edu.ph>
2025-07-17 09:23:38 +0800

    final css

--- Web/include/connect.php
+++ Web/include/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'srvan_db');
+$conn = new mysqli('localhost', 'root', '', 's24100314_businessdb');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
